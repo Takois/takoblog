7e87b81a13732ad38256791f3ecf3ff19b80890a
Takois <48357449+Takois@users.noreply.github.com>
2020-02-07 20:06:15 +0300

    Add files via upload

---
+++ takoblog/blog.py
@@ -0,0 +1,270 @@
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request,g
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+#Kullanıcı kayıt formu
+class RegisterForm(Form):
+    name=StringField("İsim Soyisim",validators=[validators.Length(min=3,max=20)])
+    username=StringField("Kullanıcı Adı",validators=[validators.Length(min=3,max=20)])
+    email=StringField("E-mail",validators=[validators.Email(message="Lütfen geçerli bir email girer misin")])
+    password=PasswordField("Şifre",validators=[
+        validators.DataRequired(message="Şifresiz mi kalıcan illa"),
+        validators.EqualTo(fieldname="confirm",message="Uyuşmuyor parolalar")
+    ])
+    confirm = PasswordField("Parola doğrula")
+class LoginForm(Form):
+    username=StringField("Kullanıcı adı")
+    password=PasswordField("Parola")
+
+app = Flask(__name__)
+app.secret_key="Takoblog"
+app.config["MYSQL_HOST"]= "localhost"
+app.config["MYSQL_USER"]= "root"
+app.config["MYSQL_PASSWORD"]= ""
+app.config["MYSQL_DB"]= "Takoblog"
+app.config["MYSQL_CURSORCLASS"]="DictCursor"
+mysql=MySQL(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about() :
+    return render_template("about.html")
+
+
+@app.route("/bc")
+def bc() :
+    return render_template("bc.html")
+
+@app.route("/articles")
+def articles():
+    cursor=mysql.connection.cursor()
+    sorgu="Select * From articles"
+    result=cursor.execute(sorgu)
+
+    if result>0:
+        articles= cursor.fetchall()
+        return render_template("articles.html",articles=articles)
+    else:
+        return render_template("articles.html")
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:       
+            return f(*args, **kwargs)
+        else:
+            flash("Giriş yapman lazım burası için","danger")
+            return redirect(url_for("login"))
+    return decorated_function
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    cursor=mysql.connection.cursor()
+    sorgu="Select * From articles where author = %s"
+    result=cursor.execute(sorgu,(session["username"],))
+
+    if result > 0:
+        articles=cursor.fetchall()
+        return render_template("dashboard.html",articles=articles)
+
+
+    return render_template("dashboard.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register() :
+    form=RegisterForm(request.form)
+    if request.method=="POST" and form.validate(): 
+        
+        name=form.name.data
+        username=form.username.data
+        email=form.email.data
+        #şifrelemek istersem 256 cruyptle
+        password=form.password.data
+
+        cursor=mysql.connection.cursor()
+        sorgu="Insert into users(name,email,username,password) VALUES(%s,%s,%s,%s)"
+        cursor.execute(sorgu,(name,email,username,password))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("helal lan sana","success")
+        return redirect(url_for("login"))      
+    
+    else:
+        return render_template("register.html",form=form)
+
+#Login işlemi  
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form=LoginForm(request.form)
+    if request.method == "POST":
+        username=form.username.data
+        password_entered=form.password.data
+        cursor=mysql.connect.cursor()
+
+        sorgu="Select * From users where username = %s"
+        result= cursor.execute(sorgu,(username,))
+
+        if result > 0:
+            data=cursor.fetchone()
+            real_password=data["password"]
+            if password_entered==real_password:
+                flash("Welcome back boss","success")
+
+                session["logged_in"]=True
+                session["username"] = username
+                return redirect(url_for("index"))
+            else:
+                flash("Yanlış şifre","danger")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Böyle bir kullanıcı yok ki","danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",form=form)
+
+@app.route("/article/<string:id>")
+def article(id):
+    cursor = mysql.connection.cursor()
+    sorgu = "Select * from articles where id = %s"
+    result = cursor.execute(sorgu,(id,))
+    if result > 0:
+        article = cursor.fetchone()
+        return render_template("article.html",article=article)
+    else:
+        return render_template("article.html")
+
+
+#logout işlemi
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+#makale silme
+@app.route("/delete/<string:id>")
+@login_required
+def delete(id):
+    cursor = mysql.connection.cursor()
+    sorgu="Select * from articles where author = %s and id= %s"
+    result=cursor.execute(sorgu,(session["username"],id))
+
+    if result>0:
+        sorgu2="Delete from articles where id = %s"
+        cursor.execute(sorgu2,(id,))
+        mysql.connection.commit()
+        return redirect(url_for("dashboard"))
+    else:
+        flash("Aga bu senin değil!","danger")
+        return redirect(url_for("index"))
+ 
+
+#adding article SONUNDA
+@app.route("/addarticle",methods=["GET","POST"])
+def addarticle():
+    form=ArticleForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        
+        title=form.title.data
+        content=form.content.data
+
+        cursor=mysql.connection.cursor()
+        sorgu="Insert into articles(title,author,content) VALUES(%s,%s,%s)"
+        cursor.execute(sorgu,(title,session["username"],content))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("Yazı eklendi O7","success")
+        return redirect(url_for("dashboard"))
+    
+    return render_template("addarticle.html",form=form)
+
+
+#updating article SONNNN
+#Makale Güncelleme
+@app.route("/edit/<string:id>",methods = ["GET","POST"])
+@login_required
+def update(id):
+   if request.method == "GET":
+       cursor = mysql.connection.cursor()
+
+       sorgu = "Select * from articles where id = %s and author = %s"
+       result = cursor.execute(sorgu,(id,session["username"]))
+
+       if result == 0:
+           flash("Böyle bir makale yok veya bu işleme yetkiniz yok","danger")
+           return redirect(url_for("index"))
+       else:
+           article = cursor.fetchone()
+           form = ArticleForm()
+
+           form.title.data = article["title"]
+           form.content.data = article["content"]
+           return render_template("update.html",form = form)
+
+   else:
+       # POST REQUEST
+       form = ArticleForm(request.form)
+
+       newTitle = form.title.data
+       newContent = form.content.data
+
+       sorgu2 = "Update articles Set title = %s,content = %s where id = %s "
+
+       cursor = mysql.connection.cursor()
+
+       cursor.execute(sorgu2,(newTitle,newContent,id))
+
+       mysql.connection.commit()
+
+       flash("Makale başarıyla güncellendi","success")
+
+       return redirect(url_for("dashboard"))
+
+
+
+class ArticleForm(Form):
+    title=StringField("Yazının başlığı:",validators=[validators.Length(min=5,max=100)])
+    content=TextAreaField("Yazının kendisi",validators=[validators.Length(min=10)])
+
+
+
+#search engine
+# Arama URL
+@app.route("/search",methods = ["GET","POST"])
+def search():
+   if request.method == "GET":
+       return redirect(url_for("index"))
+   else:
+       keyword = request.form.get("keyword")
+
+       cursor = mysql.connection.cursor()
+
+       sorgu = "Select * from articles where title like '%" + keyword +"%'"
+
+       result = cursor.execute(sorgu)
+
+       if result == 0:
+           flash("Aranan kelimeye uygun yazı bulunamadı...","warning")
+           return redirect(url_for("articles"))
+       else:
+           articles = cursor.fetchall()
+
+           return render_template("articles.html",articles = articles)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
